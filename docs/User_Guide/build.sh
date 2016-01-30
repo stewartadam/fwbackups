d85c06e08171fe7548e4dc8cab5db444bc90ad3a
Stewart Adam <s.adam@diffingo.com>
2016-01-30 17:41:47 -0500

    Update documentation version

--- docs/User_Guide/build.sh
+++ docs/User_Guide/build.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 publican clean_ids
 publican build --formats=html,html-single,pdf --langs=all
+rm -rf ../html
+rm -f ../pdf/fwbackups*.pdf
+cp -a tmp/en-US/html ../
+cp -f tmp/en-US/html-single/index.html ../html/index-single.html
+cp -f tmp/en-US/pdf/fwbackups*.pdf ../pdf
